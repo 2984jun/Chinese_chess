7c8eb8016ec07e9e3ce7f8fc4e7c0a593f0b9c8e
2984jun <2984673006@qq.com>
2023-10-08 15:00:14 +0800

    Update

--- src/Chess.java
+++ src/Chess.java
@@ -1,2 +1,182 @@
+import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
 public class Chess {
+    public static void main(String[] args){
+        new ChessMainFrame("中国象棋；观棋不语真君子，棋死无悔大丈夫");
+    }
+}
+/*ActionListener,MouseListener,*/
+class ChessMainFrame extends JFrame implements Runnable{
+    //棋子
+    JLabel play[]=new JLabel[32];
+    //棋盘
+    JLabel image;
+    //窗格
+    Container con;
+    //工具栏
+    JToolBar jmain;
+    //重新开始
+    JButton anew;
+    //悔棋
+    JButton repent;
+    //退出
+    JButton exit;
+    //当前信息
+    JLabel text;
+    //保存当前操作
+    Vector Var;
+    //规则类对象
+    ChessRule rule;
+
+    /*单击棋子
+        chessManClick=true 闪烁棋子 并给线程响应
+        chessManClick=false 吃棋子 停止闪烁 并给线程响应
+     */
+    boolean chessManClick;
+
+    /*控制玩家走棋
+        chessPlayClick=1 黑棋走棋
+        chessPlayClick=2 红棋走棋 默认红棋
+        chessPlayClick=3 双方都不能走棋
+     */
+    int chessPlayClick=2;
+
+    //控制棋子闪烁的线程
+    Thread tmain;
+    //把第一次的单击棋子给线程响应
+    static int Man,i;
+
+    ChessMainFrame(){
+        new ChessMainFrame("中国象棋");
+    }
+
+    ChessMainFrame(String Title){
+        Font font=new Font("Dialog",Font.PLAIN,12);
+        java.util.Enumeration keys= UIManager.getDefaults().keys();
+        while(keys.hasMoreElements()){
+            Object key=keys.nextElement();
+            Object value=UIManager.get(key);
+            if(value instanceof javax.swing.plaf.FontUIResource){
+                UIManager.put(key,font);
+            }
+        }
+        //获行客格引用
+        con=this.getContentPane();
+        con.setLayout(null);
+
+        //实例化规则类
+        rule=new ChessRule();
+        Var=new Vector();
+
+        //创建工具栏
+        jmain=new JToolBar();
+        text=new JLabel("欢迎使用象棋对弈系统");
+
+        //当鼠标放上显示信息
+        text.setToolTipText("信息提示");
+        anew=new JButton("新游戏");
+        anew.setToolTipText("重新开始新的一局");
+        exit=new JButton("退出");
+        exit.setToolTipText("退出象棋程序");
+        repent=new JButton("悔棋");
+        repent.setToolTipText("返回到上次走棋的位置");
+
+        //把组件添加到工具烂
+        jmain.setLayout(new GridLayout(0,4));
+        jmain.add(anew);
+        jmain.add(repent);
+        jmain.add(exit);
+        jmain.add(text);
+        jmain.setBounds(0,0,558,30);
+        con.add(jmain);
+
+        //添加棋子标签
+        drewChessMan drewChessMan_=new drewChessMan();
+
+        //注册按钮监听
+        anew.addActionListener(this);
+        repent.addActionListener(this);
+        exit.addActionListener(this);
+
+        //注册棋子移动监听
+        for(int i=0;i<32;i++){
+            con.add(play[i]);
+            //play[i].addMouseListener(this);
+        }
+
+        //添加棋盘标签
+        con.add(image=new JLabel(new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\R-C.jpg")));
+        image.setBounds(0,30,558,620);
+        //image.addMouseListener(this);
+
+        //注册窗体关闭监听
+        this.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we){
+                System.exit(0);
+            }
+        });
+
+        //窗体居中及一些基本设置
+        Dimension screenSize=Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension frameSize=this.getSize();
+
+        if(frameSize.height>screenSize.height){
+            frameSize.height=screenSize.height;
+        }
+        if(frameSize.width>screenSize.width){
+            frameSize.width=screenSize.width;
+        }
+
+        this.setLocation((screenSize.width- frameSize.width)/2-280,(screenSize.height-frameSize.height)/2-350);
+
+        //设置
+        this.setIconImage(new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\b151f8198618367afb3ba3a42d738bd4b31ce51d.jpg").getImage());
+        this.setIconImage(new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\b151f8198618367afb3ba3a42d738bd4b31ce51d.jpg").getImage());
+        this.setResizable(false);
+        this.setTitle(Title);
+        this.setSize(558,670);
+        this.show();
+    }
+
+    public void run(){
+        while(true){
+            if(chessManClick){
+                play[Man].setVisible(false);
+                try{
+                    tmain.sleep(200);
+                }catch (Exception e){
+                    play[Man].setVisible(true);
+                }
+            }else{
+                text.setVisible(false);
+                try{
+                    tmain.sleep(250);
+                }catch (Exception e){
+                    text.setVisible(true);
+                }
+            }
+            try{
+                tmain.sleep(350);
+            }catch (Exception e){
+            }
+        }
+    }
+
+    public void mouseClicked(MouseEvent me){
+        System.out.println("Mouse");
+
+        //当前坐标
+        int Ex=0,Ey=0;
+        //启动线程
+        if(tmain==null){
+            tmain=new Thread(this);
+            tmain.start();
+        }
+    }
 }
---
+++ src/drewChessMan.java
@@ -0,0 +1,122 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class drewChessMan extends JFrame{
+    public void drewChessMan(JLabel play[]){
+        //流程控制
+        int i,k;
+        //图标
+        Icon in;
+
+        //黑色棋子
+
+        //车
+        in=new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\a7af275077614853a7b552db580e8ee4.png");
+        for(i=0,k=24;i<2;i++,k+=456){
+            play[i]=new JLabel(in);
+            play[i].setBounds(k,56,55,55);
+            play[i].setName("黑车");
+        }
+
+        //马
+        in=new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\10218cb37ccc42e58c0ea1bbdec503fd.png");
+        for(i=4,k=81;i<6;i++,k+=342){
+            play[i]=new JLabel(in);
+            play[i].setBounds(k,56,55,55);
+            play[i].setName("黑马");
+        }
+
+        //象
+        in=new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\0fbe8b946eca4351b6929d0c46517e8f.png");
+        for(i=8,k=138;i<10;i++,k+=228){
+            play[i]=new JLabel(in);
+            play[i].setBounds(k,56,55,55);
+            play[i].setName("象");
+        }
+
+        //士
+        in=new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\0b43a15126a7454ea92f852a135c9e41 (1).png");
+        for(i=12,k=195;i<14;i++,k+=114){
+            play[i]=new JLabel(in);
+            play[i].setBounds(k,56,55,55);
+            play[i].setName("士");
+        }
+
+        //卒
+        in=new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\27f55f020f9440e7873e0074d3aac7cd.png");
+        for(i=16,k=24;i<21;i++,k+=114){
+            play[i]=new JLabel(in);
+            play[i].setBounds(k,227,55,55);
+            play[i].setName("卒");
+        }
+
+        //炮
+        in=new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\bd8f4b1a88934f8086acc9ba2e56d2a7.png");
+        for(i=26,k=81;i<28;i++,k+=342){
+            play[i]=new JLabel(in);
+            play[i].setBounds(k,170,55,55);
+            play[i].setName("黑炮");
+        }
+
+        //将
+        in=new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\be8c1ad4c2904b51b91ec33c17922f1c.png");
+        play[30]=new JLabel(in);
+        play[30].setBounds(252,56,55,55);
+        play[30].setName("将");
+
+        //红色棋子
+        //车
+        in=new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\屏幕截图 2023-10-08 140110.png");
+        for(i=2,k=24;i<4;i++,k+=456){
+            play[i]=new JLabel(in);
+            play[i].setBounds(k,569,55,55);
+            play[i].setName("红车");
+        }
+
+        //马
+        in=new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\屏幕截图 2023-10-08 140102.png");
+        for(i=6,k=81;i<8;i++,k+=342){
+            play[i]=new JLabel(in);
+            play[i].setBounds(k,569,55,55);
+            play[i].setName("红马");
+        }
+
+        //相
+        in=new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\屏幕截图 2023-10-08 140052.png");
+        for(i=10,k=138;i<12;i++,k+=228){
+            play[i]=new JLabel(in);
+            play[i].setBounds(k,569,55,55);
+            play[i].setName("相");
+        }
+
+        //仕
+        in=new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\ba799e0475b949b8b454b0a054b8d17b.png");
+        for(i=14,k=195;i<16;i++,k+=114){
+            play[i]=new JLabel(in);
+            play[i].setBounds(k,569,55,55);
+            play[i].setName("仕");
+        }
+
+        //兵
+        in=new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\17ca8c1963964d1a91cbe47a6e2784a8.png");
+        for(i=21,k=24;i<26;i++,k+=114){
+            play[i]=new JLabel(in);
+            play[i].setBounds(k,398,55,55);
+            play[i].setName("兵");
+        }
+
+        //炮
+        in=new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\3519df795efe46cd9226a1908625db35.png");
+        for(i=28,k=81;i<30;i++,k+=342){
+            play[i]=new JLabel(in);
+            play[i].setBounds(k,455,55,55);
+            play[i].setName("红炮");
+        }
+
+        //帅
+        in=new ImageIcon("C:\\Users\\fuwujun\\Pictures\\Camera Roll\\b151f8198618367afb3ba3a42d738bd4b31ce51d.jpg");
+        play[31]=new JLabel(in);
+        play[31].setBounds(252,560,55,55);
+        play[33].setName("帅");
+    }
+}
